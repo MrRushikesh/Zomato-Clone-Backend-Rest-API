09b719df421e717d36334128200c8ded8609ab07
MrRushikesh <rusikeshingale7447@gmail.com>
2024-02-14 17:45:23 +0530

    add try or catch block in controller.js

--- controller/controller.js
+++ controller/controller.js
@@ -1,8 +1,8 @@
 let mongodb = require('mongodb');
 let {MongoClient} = require('mongodb');
 
-// let mongoUrl = `mongodb://127.0.0.1:27017/`;
-let mongoUrl = `mongodb+srv://rushikeshingale7447:nxuAikDE0tzaQ024@cluster0.dnwndyi.mongodb.net/`
+let mongoUrl = `mongodb://127.0.0.1:27017/`;
+// let mongoUrl = `mongodb+srv://rushikeshingale7447:nxuAikDE0tzaQ024@cluster0.dnwndyi.mongodb.net/`
 
 let client = new MongoClient(mongoUrl);
 
@@ -30,13 +30,15 @@ async function getData(collectionName, query){
         for await(const data of cursor){
             output.push(data);
         }
-       cursor.closed()
-
+        return output;
+        // cursor.closed()
     }catch(err){
+        // console.log("Error While Getting Data")
         output.push({"Error" : "Error While Getting Data"},err)
+       
     }
 
-    return output;
+    // return output;
 }
 
 // POST 
@@ -46,11 +48,13 @@ async function postData (collectionName, data){
 
     try{
         output = await db.collection(collectionName).insertOne(data);
+        return output;
     }catch(err){
+        // console.log("Error While Getting Data")
         output = {"Error" : "Error while posting data"}
     }
     
-    return output;
+    // return output;
 
 }
 
@@ -60,11 +64,12 @@ async function updateOrder (collectionName, condition, data){
 
     try{
         output = await db.collection(collectionName).updateOne(condition,data);
+        return output;
     }catch(err){
         output = {"Error":"Error While Updating the data..."}
     }
 
-    return output;
+    // return output;
 
 }
 
@@ -73,11 +78,12 @@ async function deleteOrder (collectionName, condition){
     let output;
     try{
         output = await db.collection(collectionName).deleteOne(condition);
+        return output;
     }catch(err){
         output = {"Error" : "Error While Deleting the data"}
     }
 
-    return output;
+    // return output;
 }
 
 module.exports = {dbConnection, getData, postData, updateOrder, deleteOrder};
